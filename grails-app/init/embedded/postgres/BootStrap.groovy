b0f780c8df6ddcbdb93cceb1f1c0a60bd09094eb
alexey <a.chentsov@avax.de>
2018-03-21 01:51:53 +0100

    Cleared all redundant artifacts. Added the main embedded definition.

--- grails-app/init/embedded/postgres/BootStrap.groovy
+++
@@ -1,9 +0,0 @@
-package embedded.postgres
-
-class BootStrap {
-
-    def init = { servletContext ->
-    }
-    def destroy = {
-    }
-}
--- src/main/groovy/embedded/postgres/EmbeddedPostgresGrailsPlugin.groovy
+++ src/main/groovy/embedded/postgres/EmbeddedPostgresGrailsPlugin.groovy
@@ -1,70 +1,31 @@
 package embedded.postgres
 
-import grails.plugins.*
+import grails.plugins.Plugin
 
 class EmbeddedPostgresGrailsPlugin extends Plugin {
-
-    // the version or versions of Grails the plugin is designed for
-    def grailsVersion = "3.3.3 > *"
-    // resources that are excluded from plugin packaging
-    def pluginExcludes = [
-        "grails-app/views/error.gsp"
-    ]
-
-    // TODO Fill in these fields
-    def title = "Embedded Postgres" // Headline display name of the plugin
-    def author = "Your name"
-    def authorEmail = ""
-    def description = '''\
-Brief summary/description of the plugin.
-'''
-    def profiles = ['web']
-
-    // URL to the plugin's documentation
-    def documentation = "http://grails.org/plugin/embedded-postgres"
-
-    // Extra (optional) plugin metadata
-
-    // License: one of 'APACHE', 'GPL2', 'GPL3'
-//    def license = "APACHE"
-
-    // Details of company behind the plugin (if there is one)
-//    def organization = [ name: "My Company", url: "http://www.my-company.com/" ]
-
-    // Any additional developers beyond the author specified above.
-//    def developers = [ [ name: "Joe Bloggs", email: "joe@bloggs.net" ]]
-
-    // Location of the plugin's issue tracker.
-//    def issueManagement = [ system: "JIRA", url: "http://jira.grails.org/browse/GPMYPLUGIN" ]
-
-    // Online location of the plugin's browseable source code.
-//    def scm = [ url: "http://svn.codehaus.org/grails-plugins/" ]
+    def grailsVersion   = "3.2.10 > *"
+    def title           = "Embedded Postgres"
+    def author          = "Alexey Chentsov"
+    def authorEmail     = "alche8411@gmail.com"
+    def description     = 'Plugin starts local instance of postgres and after program termination clears temporary data. Simulates embedded postgres.'
+    def documentation   = "https://github.com/Relaximus/embedded-postgres-grails-plugin"
+    def license         = "APACHE"
+    def organization    = [name: "Alexey Chentsov", url: "https://github.com/Relaximus"]
+    def issueManagement = [ system: "GITHUB", url: "https://github.com/Relaximus/embedded-postgres-grails-plugin/issues" ]
+    def scm             = [ url: "https://github.com/Relaximus/embedded-postgres-grails-plugin" ]
+    def pluginExcludes  = []
+    def developers      = [ [name: 'Alexey Chentsov'] ]
+    def dependsOn = [dataSource: '*']
 
     Closure doWithSpring() { {->
-            // TODO Implement runtime spring config (optional)
+        def config = grailsApplication.config
+        if (config.dataSource.embeddedPostgres) {
+            embeddedPostgresFactory(EmbeddedPostgresFactory){
+                port = config.dataSource?.embeddedPort
+            }
+            embeddedPostgres(embeddedPostgresFactory: "getDataBase")
+            getBeanDefinition('dataSource').setDependsOn('embeddedPostgres')
         }
-    }
-
-    void doWithDynamicMethods() {
-        // TODO Implement registering dynamic methods to classes (optional)
-    }
-
-    void doWithApplicationContext() {
-        // TODO Implement post initialization spring config (optional)
-    }
-
-    void onChange(Map<String, Object> event) {
-        // TODO Implement code that is executed when any artefact that this plugin is
-        // watching is modified and reloaded. The event contains: event.source,
-        // event.application, event.manager, event.ctx, and event.plugin.
-    }
-
-    void onConfigChange(Map<String, Object> event) {
-        // TODO Implement code that is executed when the project configuration changes.
-        // The event is the same as for 'onChange'.
-    }
-
-    void onShutdown(Map<String, Object> event) {
-        // TODO Implement code that is executed when the application shuts down (optional)
+        }
     }
 }
